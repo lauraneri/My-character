493771dc0752cad9de838260cbfbfd34dd103d07
paroxitona <78615562+paroxitona@users.noreply.github.com>
2021-02-05 15:59:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Bem-Vindo ao MyCharacter!</title>
+	<link rel="stylesheet" type="text/css" href="EstiloPaginaInicial.css">
+	<link rel="icon" type="image/png" href="fenix.png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+		<div class="container-fluid     float-md-left" id="BarraInicial">
+  			<div class="row">
+    			<div class="col" style="margin-top: 16px;">
+      				<img src="fenix.png">
+    			</div>
+    			<div class="col" style="margin-top: 30px;">
+     				<form>
+  							<div class="row">
+  								<p id="Label">Email</p>
+    							<div class="col">
+      								<input type="text" class="form-control" placeholder="Email">
+    							</div>
+    							<label id="Label">Senha</label>
+    							<div class="col">
+      								<input type="password" class="form-control" placeholder="Senha">
+    							</div>
+    							<div class="col">
+      								<button type="button" class="btn btn-primary">Entrar</button>
+    							</div>
+  							</div>
+					</form>
+    			</div>
+  			</div>
+		<button type="button" class="btn btn-link" style="float: right;"> <a href="Cadastro.php">Novo por aqui? Cadastre-se</a> | Esqueci minha senha</button>
+		<h1 id="fonte">My Character</h1>
+		<img src="imgem.png" class="img-fluid" alt="Imagem responsiva" >
+		<img src="estrela1.png" id="estrela1_flutuante" class="img-fluid" alt="Imagem responsiva">
+		<p id="fraseEfeito">Onde seus <br>personagens <br> ganham vida</p>
+		<img src="estrela2.png" id="estrela2_flutuante" class="img-fluid" alt="Imagem responsiva">
+</body>
+</html>
